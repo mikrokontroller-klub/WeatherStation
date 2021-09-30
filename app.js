0bb405ed56b52450113c40afd4fc53df4ace988d
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-09-30 12:18:04 +0200

    Create static viewes based on wireframe

--- app.js
+++ app.js
@@ -6,15 +6,47 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.redirect('/home');
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/template.html'));
+    res.sendFile(path.join(__dirname, 'views/home.html'));
 });
 
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/dashboard.html'));
+app.get('/sensors', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/sensors/index.html'));
+});
+
+app.get('/sensors/new', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/sensors/new.html'));
+});
+
+app.get('/sensors/:id/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/sensors/edit.html'));
+});
+
+app.get('/sensors/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/sensors/view.html'));
+});
+
+app.get('/users', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/users/index.html'));
+});
+
+app.get('/users/new', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/users/new.html'));
+});
+
+app.get('/users/:id/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/users/edit.html'));
+});
+
+app.get('/users/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/users/view.html'));
 });
 
 const port = process.env.PORT || 8080;
