3aafd6a321d7cc32d53e98233f1197b050ef39ce
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-10-24 10:15:54 +0200

    [WIP]: Refactor views, create templates

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const chalk = require('chalk');
 const { routes } = require('./routers/routes');
 const { apiRoutes } = require('./routers/routesApi');
 const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
 const { baseController } = require('./controllers/baseController');
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/app');
 
 app.use(
     session({
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,6 +5,7 @@ const path = require('path');
 exports.homeController = {
     /** Display a listing of the resource. */
     index: async (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/home.html'));
+        //res.sendFile(path.join(__dirname, '../views/home.html'));
+        res.render('pages/home/index');
     },
 };
