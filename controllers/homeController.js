553e0d3ff19173a08e919f240a9f96697e0806d4
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-10-28 15:07:30 +0200

    Load home page data from server

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,65 @@
-const path = require('path');
+const moment = require('moment');
+
 /**
  * @description
  */
 exports.homeController = {
     /** Display a listing of the resource. */
     index: async (req, res) => {
-        //res.sendFile(path.join(__dirname, '../views/home.html'));
-        res.render('pages/home/index', { activePage: 'home' });
+        let lastMeasurements = [
+            {
+                id: 1,
+                name: 'My Temperature Sensor',
+                type: 'Temperature',
+                icon: {
+                    name: 'fas fa-thermometer-half',
+                    color: 'warning',
+                },
+                measurement: {
+                    measuredAt: moment().subtract(10, 'minutes'),
+                    data: 15.67,
+                    unitName: 'Celsius',
+                    unitPostfix: 'C°',
+                },
+            },
+            {
+                id: 2,
+                name: 'My Humidity Sensor',
+                type: 'Humidity',
+                icon: {
+                    name: 'fas fa-tint',
+                    color: 'info',
+                },
+                measurement: {
+                    measuredAt: moment().subtract(15, 'minutes'),
+                    data: 83.2,
+                    unitName: 'Humidity',
+                    unitPostfix: '%',
+                },
+            },
+        ];
+        let sensorMeasurements = [
+            {
+                id: 2,
+                name: 'My Humidity Sensor',
+                type: 'Humidty',
+                color: 'info',
+                measurements: {
+                    data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 40)),
+                    labels: Array.from({ length: 20 }, () => Math.floor(Math.random() * 40)),
+                },
+            },
+            {
+                id: 3,
+                name: 'My Pressure Sensor',
+                type: 'Pressure',
+                color: 'danger',
+                measurements: {
+                    data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 40)),
+                    labels: Array.from({ length: 20 }, () => Math.floor(Math.random() * 40)),
+                },
+            },
+        ];
+        res.render('pages/home/index', { activePage: 'home', lastMeasurements, sensorMeasurements, moment });
     },
 };
